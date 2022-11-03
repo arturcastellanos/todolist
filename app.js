a1092064ff05f902084185ddcc4bd14251577c8d
Artur Castellanos <108274411+arturcastellanos@users.noreply.github.com>
2022-11-03 17:30:10 +0200

    add app.js and index.html

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    
+    var today = new Date();
+
+    if (today.getDay() === 6 || today.getDay() === 0) {
+        res.send("Yay it's the weekend")
+    } else {
+        res.send("Boo I have to work")
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
